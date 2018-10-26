5c43f41cade8b6baab0875a03fb7935999806b33
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2018-10-26 19:12:11 +0300

    Improved error handling in minio module
    Change-Id: I6096a0974a60c5497da00b0c0ec111750e06babb

--- tests/integration/modules/minio.py
+++ tests/integration/modules/minio.py
@@ -3,25 +3,57 @@ Interface to Minio S3 server.
 """
 import json
 
-from tenacity import retry, stop_after_attempt, wait_fixed
+from tenacity import (retry, retry_if_exception_type, stop_after_attempt,
+                      wait_fixed)
 
 from . import docker
 
 
-@retry(wait=wait_fixed(0.5), stop=stop_after_attempt(360))
+class MinioException(Exception):
+    """
+    Minion exception.
+    """
+
+    def __init__(self, response):
+        super().__init__(self._fmt_message(response))
+        self.response = response
+
+    @staticmethod
+    def _fmt_message(response):
+        try:
+            error = response['error']
+            message = '{0} Cause: {1}'.format(error['message'],
+                                              error['cause']['message'])
+
+            code = error['cause']['error'].get('Code')
+            if code:
+                message = '{0} [{1}]'.format(message, code)
+
+            return message
+
+        except Exception:
+            return 'Failed with response: {0}'.format(response)
+
+
+class BucketAlreadyOwnedByYou(MinioException):
+    """
+    BucketAlreadyOwnedByYou Minion exception.
+    """
+
+
+@retry(
+    retry=retry_if_exception_type(MinioException),
+    wait=wait_fixed(0.5),
+    stop=stop_after_attempt(360))
 def configure_s3_credentials(context):
     """
     Configure S3 credentials in mc (Minio client).
     """
     access_key = context.conf['s3']['access_key_id']
-    secret_key = bucket = context.conf['s3']['access_secret_key']
-
-    response = _mc_execute(
+    secret_key = context.conf['s3']['access_secret_key']
+    _mc_execute(
         context, 'config host add local http://localhost:9000 {0} {1}'.format(
             access_key, secret_key))
-    if response['status'] != 'success':
-        raise RuntimeError('Cannot configure s3 credentials {0}: {1}'.format(
-            bucket, response))
 
 
 def ensure_s3_bucket(context):
@@ -29,13 +61,10 @@ def ensure_s3_bucket(context):
     Ensure S3 has the bucket specified in the config.
     """
     bucket = context.conf['s3']['bucket']
-
-    response = _mc_execute(context, 'mb local/{0}'.format(bucket))
-    if response['status'] != 'success':
-        error_code = response['error']['cause']['error']['Code']
-        if error_code != 'BucketAlreadyOwnedByYou':
-            raise RuntimeError('Cannot create bucket {0}: {1}'.format(
-                bucket, response))
+    try:
+        _mc_execute(context, 'mb local/{0}'.format(bucket))
+    except BucketAlreadyOwnedByYou:
+        pass
 
 
 def _mc_execute(context, command):
@@ -43,5 +72,15 @@ def _mc_execute(context, command):
     Execute mc (Minio client) command.
     """
     container = docker.get_container(context, context.conf['s3']['container'])
+
     output = container.exec_run('mc --json {0}'.format(command)).decode()
-    return json.loads(output)
+
+    response = json.loads(output)
+    if response['status'] == 'success':
+        return response
+
+    error_code = response['error']['cause']['error'].get('Code')
+    exception_types = {
+        'BucketAlreadyOwnedByYou': BucketAlreadyOwnedByYou,
+    }
+    raise exception_types.get(error_code, MinioException)(response)
