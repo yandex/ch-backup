664f21357a7c4c659bafbe7c75cd79cea402627a
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2023-08-06 23:23:20 +0300

    Use S3 fault injection only in dedicated test

--- images/proxy-api/app.py
+++ images/proxy-api/app.py
@@ -6,12 +6,12 @@ import bottle
 
 @bottle.route("/")
 def index():
-    network = str(os.environ.get("NETWORK"))
+    network = os.environ.get("NETWORK")
     # 10% chance to get broken proxy.
-    if random.randint(0, 9) > 0:
-        return "proxy01." + network
+    if not os.environ.get("S3_FAULT_INJECTION_ENABLED") or random.randint(0, 9) > 0:
+        return f"proxy01.{network}"
     else:
-        return "broken-proxy01." + network
+        return f"broken-proxy01.{network}"
 
 
 bottle.run(host="0.0.0.0", port=8080)
