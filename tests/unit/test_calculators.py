a6dd89d8980dccc742c30de2538a7ae8d89bfddf
Кирилл Гарбар <st087492@student.spbu.ru>
2024-03-25 13:06:31 +0300

    Default TemporaryDirectory args

--- tests/unit/test_calculators.py
+++ tests/unit/test_calculators.py
@@ -76,7 +76,7 @@ def test_calc_tarball_size(
     # Assuming name_lens size is less than 10
     names = [f"{i}" * name_len for i, name_len in enumerate(name_lens)]
     cwd = Path(__file__).parent.resolve()
-    with TemporaryDirectory(prefix=str(cwd) + "/") as dir_name:
+    with TemporaryDirectory() as dir_name:
         try:
             dir_path = Path(dir_name)
             for name in names:
