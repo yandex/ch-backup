0c3e4fd2b6698af02643f80e44cf481bacee813b
Кирилл Гарбар <st087492@student.spbu.ru>
2024-08-20 19:24:26 +0200

    Skip s3 frozen parts

--- ch_backup/storage/async_pipeline/stages/backup/deduplicate_stage.py
+++ ch_backup/storage/async_pipeline/stages/backup/deduplicate_stage.py
@@ -13,6 +13,8 @@ from ch_backup.clickhouse.models import Database, FrozenPart, Table
 from ch_backup.storage.async_pipeline.base_pipeline.handler import IterableHandler
 from ch_backup.storage.async_pipeline.stages.types import StageType
 
+import ch_backup.storage.async_pipeline.stages.backup.stage_communication as stage_communication
+
 
 class DeduplicateStage(IterableHandler):
     """ """
@@ -26,10 +28,14 @@ class DeduplicateStage(IterableHandler):
         self.frozen_parts_batch: Dict[str, FrozenPart] = {}
         self.dedup_batch_size = config["deduplication_batch_size"]
 
-    def __call__(self, value: Tuple[str, FrozenPart], index: int) -> Optional[Iterable[FrozenPart]]:
+    def __call__(
+        self, value: Tuple[str, FrozenPart], index: int
+    ) -> Optional[Iterable[FrozenPart]]:
         disk_type, frozen_part = value
         if disk_type == "s3":
-            yield frozen_part
+            stage_communication.part_metadata_queue.put(
+                PartMetadata.from_frozen_part(frozen_part)
+            )
         else:
             self.frozen_parts_batch[frozen_part.name] = frozen_part
             if len(self.frozen_parts_batch) >= self.dedup_batch_size:
@@ -42,5 +48,5 @@ class DeduplicateStage(IterableHandler):
             for fpart in self.deduplicate_batch():
                 yield fpart
 
-    def deduplicate_batch(self): #-> Dict[str, PartMetadata]:
+    def deduplicate_batch(self):  # -> Dict[str, PartMetadata]:
         return self.frozen_parts_batch.values()
