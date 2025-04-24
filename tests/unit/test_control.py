055e6113518a7fca999a23110026af85fef4dc71
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2025-04-24 10:54:16 +0300

    Fix double-escaping in get_tables() function

---
+++ tests/unit/test_control.py
@@ -0,0 +1,37 @@
+from tests.unit.utils import parametrize
+
+from ch_backup.clickhouse.control import _format_string_array
+
+
+@parametrize(
+    {
+        "id": "empty list",
+        "args": {
+            "value": [],
+            "result": "[]",
+        },
+    },
+    {
+        "id": "single-item list",
+        "args": {
+            "value": ["value"],
+            "result": "['value']",
+        },
+    },
+    {
+        "id": "multi-item list",
+        "args": {
+            "value": ["value1", "value2"],
+            "result": "['value1','value2']",
+        },
+    },
+    {
+        "id": "escaping",
+        "args": {
+            "value": ["`for`.bar"],
+            "result": r"['\`for\`.bar']",
+        },
+    },
+)
+def test_format_string_array(value, result):
+    assert _format_string_array(value) == result
