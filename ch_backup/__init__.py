9b9ed1e4b84ec28b7bfac8ce4417dea9fe0fbf5e
Alexander Burmak <Alex-Burmak@yandex-team.ru>
2025-05-27 15:20:10 +0300

    Fix integration tests (#247)

--- ch_backup/__init__.py
+++ ch_backup/__init__.py
@@ -2,5 +2,11 @@
 ClickHouse backup tool.
 """
 
+import warnings
+
+# Ignore warnings from dependencies (stopit) on usage of deprecated pkg_resources
+# https://setuptools.pypa.io/en/latest/pkg_resources.html#api-reference
+warnings.filterwarnings("ignore", message="pkg_resources is deprecated as an API")
+
 from .cli import cli as main
 from .version import __version__
