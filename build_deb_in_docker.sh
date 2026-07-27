f497d9f409058b7d2c79384ca80d9a97a84ed7da
Aleksei Filatov <aalexfvk@gmail.com>
2026-07-27 16:06:48 +0300

    Use yandex mirror

--- build_deb_in_docker.sh
+++ build_deb_in_docker.sh
@@ -15,6 +15,9 @@ if [[ -n "${DEB_BUILD_DISTRIBUTION}" ]]; then
     BUILD_ARGS+=(--build-arg BASE_IMAGE=${DEB_BUILD_DISTRIBUTION})
     BUILD_IMAGE="${BUILD_IMAGE}-${DEB_BUILD_DISTRIBUTION}"
 fi
+if [[ -n "${USE_YANDEX_MIRROR}" ]]; then
+    BUILD_ARGS+=(--build-arg USE_YANDEX_MIRROR=${USE_YANDEX_MIRROR})
+fi
 # Normalize docker image name
 BUILD_IMAGE=$(echo ${BUILD_IMAGE} | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9._-]/-/g')
 
