84526ebb3f1f0954a50291eacfd3b2516290f120
Dmitriy Borisenko <otselnik@yandex.ru>
2026-09-03 07:41:01 +0300

    Fix ClickHouse 26.8 configuration compatibility (#352)
    * Remove obsolete TCP SSL port setting
    
    * Stop exposing unused secure TCP port
    
    * Keep disks history setting out of server config
    
    * Update cloud object count for ClickHouse 26.8

--- tests/unit/test_disks.py
+++ tests/unit/test_disks.py
@@ -3,6 +3,7 @@ Unit tests disks module.
 """
 
 import unittest
+import unittest.mock
 from typing import List, Optional
 
 import xmltodict
@@ -60,7 +61,6 @@ write_result = ""
                     </object_storage>
                   </disks>
                 </storage_configuration>
-                <history-file>/tmp/.disks-file-history</history-file>
               </clickhouse>
               """,
         },
@@ -106,7 +106,6 @@ write_result = ""
                     </object_storage>
                   </disks>
                 </storage_configuration>
-                <history-file>/tmp/.disks-file-history</history-file>
               </clickhouse>
               """,
         },
@@ -149,7 +148,6 @@ write_result = ""
                     </object_storage_source>
                   </disks>
                 </storage_configuration>
-                <history-file>/tmp/.disks-file-history</history-file>
               </clickhouse>
               """,
         },
@@ -261,10 +259,20 @@ def test_enter_without_storage_configuration():
     """
     disk_manager = _make_temporary_disks(clickhouse_config_xml, cloud_storage_disks=[])
 
-    with unittest.mock.patch("builtins.open", new=unittest.mock.mock_open()):
+    # pylint: disable=global-statement
+    global write_result
+    write_result = ""
+    with unittest.mock.patch("builtins.open", new=unittest.mock.mock_open()) as m:
+        m().write = write_collector
         with disk_manager:
             # pylint: disable=protected-access
             assert disk_manager._disks == {}
+
+    actual_content = xmltodict.parse(write_result, disable_entities=False)
+    assert_equal(
+        actual_content["clickhouse"]["history-file"],
+        "/tmp/.disks-file-history",
+    )
 
 
 def test_create_temporary_disk_missing_disk_raises():
